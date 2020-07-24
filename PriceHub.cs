88a49c1a0dc55b5ae354c4f22285aa51c9d870d2
tehjinxi <henryteh.1013@gmail.com>
2020-07-25 00:45:42 +0800

    simple send msg without any function in auction page

--- PriceHub.cs
+++ PriceHub.cs
@@ -2,32 +2,14 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
-namespace ChatRT
+namespace AssignmentApp
 {
-    public class ChatHub : Hub
+    public class PriceHub : Hub
     {
-        private static int count = 0;
-
         public async Task SendText(string name, string message)
         {
             await Clients.Caller.SendAsync("ReceiveText", name, message, "caller");
             await Clients.Others.SendAsync("ReceiveText", name, message, "others");
         }
-
-        public override async Task OnConnectedAsync()
-        {
-            count++;
-            string name = Context.GetHttpContext().Request.Query["name"];
-            await Clients.All.SendAsync("UpdateStatus", count, $"<b>{name}</b> joined");
-            await base.OnConnectedAsync();
-        }
-
-        public override async Task OnDisconnectedAsync(Exception exception) 
-        {
-            count--;
-            string name = Context.GetHttpContext().Request.Query["name"];
-            await Clients.All.SendAsync("UpdateStatus", count, $"<b>{name}</b> left");
-            await base.OnDisconnectedAsync(exception);
-        }       
     }
 }
