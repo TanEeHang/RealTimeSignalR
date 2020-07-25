b973901bbfb1a63384990de40d87725cfaa3c9f9
ehangtan <taneh-pm17@student.tarc.edu.my>
2020-07-25 15:39:27 +0800

    Create Room ID function implemented

--- PriceHub.cs
+++ PriceHub.cs
@@ -1,11 +1,29 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
 namespace AssignmentApp
 {
+     public class Room
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+    }
+
     public class PriceHub : Hub
     {
+        private static List<Room> rooms = new List<Room>()
+        {
+            
+        };
+
+        public string Create()
+        {
+            var room = new Room();
+            rooms.Add(room);
+            return room.Id;
+        }
+        
         public async Task SendText(string name, string message)
         {
             await Clients.Caller.SendAsync("ReceiveText", name, message, "caller");
